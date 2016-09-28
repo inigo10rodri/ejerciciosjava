48eb88b2493811b5dc5143e3ff127c58d539f3e3
DW31 <ik012982i9@H012982.NET>
2016-09-28 13:27:29 +0200

    capicua

---
+++ Workspace/Ejercicios2/src/Ejercicio9.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ejercicio9 {
+//Pedir un número de cuatro cifras y decir si es capicúa.
+	public static void main(String[] args) {
+		//Definimos nuestras variables
+		int numero, falta, numInvertido, resto;
+		//iniciar el scanner
+		Scanner sc=new Scanner(System.in);
+		/*Además del número necesitamos otras variables, 
+		 numero Invertido que almacenará el número dado la vuelta, resto, 
+		 donde dejaremos los restos de las divisiones y falta, que es la variable
+		 con los números que faltan para dar la vuelta.*/
+		//Pedimos un numero al usuario
+		numero=sc.nextInt();
+		//Inicializamos las tres variables
+		while(numero<=0);{
+		  falta=numero;
+		  numInvertido=0;
+		  resto=0;}
+
+	}
+//http://lineadecodigo.com/java/saber-si-un-numero-es-capicua-con-java/
+}
