caa11fc3dc4799917a995a05cca193127966c821
DW31 <ik012982i9@H012982.NET>
2016-10-31 13:11:53 +0100

    Ejercicio notas acabado

--- Workspace/Ejercicios2/src/ejercicioswhile/Notas.java
+++ Workspace/Ejercicios2/src/ejercicioswhile/Notas.java
@@ -4,30 +4,41 @@ public class Notas {
 
 	public static void main(String[] args) {
 		//Variables
-		double nota=0;
-		int contador=0;
+		double nota;
+		int contaprobados=0;
+		int contsuspensos=0;
+		double suma=0.0;		
 		//Iniciar Scanner
 		Scanner sc=new Scanner(System.in);
 		do{
-			contador++;
-			System.out.print("Introduce una nota: ");			
-			nota=sc.nextInt();
-			if(nota<=10 & nota>=0){
-			if(nota>=5 && nota<=10){
-				contador++;
-				System.out.print("El numero de aprobados son: "+contador);
-			}
-			if(nota<=4 && nota>=0){
-				contador++;
-				System.out.print("El numero de suspensos son: "+contador);
-			}}
-			
-			
-			
-			
+			//pedir nota
+			System.out.println("Introduce una nota: (Ingrese una nota negativa para terminar!)");
+			//leer nota
+			nota=sc.nextDouble();			
+			 //Cuando el numero sea mayor que 10 que introduzca un numero entre 0 y 10
+		    if (nota>10){
+		    	System.out.print("Introduce un numero entre 0 y 10\n");
+		    }//if
+		  //Si la nota esta entre 5 y menor o igual que 10 habra aprobado e incrementamos en el contador aprobados
+			else if(nota>=5){
+		    	contaprobados++;		
+		    	suma=nota+suma;
+		    	}//else if
+		    //Si la nota esta entre 0 y menor que 4 habra suspendido e incrementamos en el contador suspensos
+		    else if(nota>=0){
+		    	contsuspensos++;
+		    	suma=suma+nota;
+			}//else if		   
+			//Indicamos que ha salido correctamente
+			else{
+				System.out.print("Ha salido correctamente!\n");
+			}//else			
 		}while(nota>=0);
-		
+		//Mostrar por pantalla el resultado final
+		System.out.print("El numero de aprobados son: "+contaprobados+"\n");
+		System.out.print("El numero de suspensos son: "+contsuspensos+"\n");
+		System.out.print("La media es: "+(suma/(contsuspensos+contaprobados)));
 
-	}
+	}//main
 
-}
+}//class
