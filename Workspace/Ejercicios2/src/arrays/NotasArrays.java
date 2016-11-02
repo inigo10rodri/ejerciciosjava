47da8c6945ff47a3f3e83f04d42117b85942bf2b
DW31 <ik012982i9@H012982.NET>
2016-11-02 13:49:36 +0100

    Arrays acabado

--- Workspace/Ejercicios2/src/arrays/NotasArrays.java
+++ Workspace/Ejercicios2/src/arrays/NotasArrays.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class NotasArrays {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		// TODO Auto-generated method stub
 		//String nombre;
 		//String[] TotAsignaturas;
 		//int[] TotAlumnos;
 		int[] TotNotas=new int [10];
-		int suma=0;
-		Scanner sc=new Scanner(System.in);
+		int suma=0;		
 		for(int i=0;i<TotNotas.length;i++){
 			System.out.print("Nota "+i+":");
 			int nota =sc.nextInt();
 			TotNotas[i]= nota;
-			
-			//calcular la suma de las notas
-			suma=suma+nota;			
 		}//for
-
+		//calcular la suma de las notas
+		for(int j=0;j<TotNotas.length;j++){
+			suma+=TotNotas[j];				
+		}//for
 		System.out.println("La suma de las notas es: "+suma);
+		System.out.println("La nota media es: "+(double) suma/TotNotas.length);
 	}//main
 
 }//class
