65e193bfa2d7081e08d4ddb1409e43edb4f49e1d
DW31 <ik012982i9@H012982.NET>
2016-11-07 13:58:11 +0100

    holii

--- Workspace/Ejercicios2/src/arrays/VerificarNumero.java
+++ Workspace/Ejercicios2/src/arrays/VerificarNumero.java
@@ -7,7 +7,7 @@ public class VerificarNumero {
 	public static void main(String[] args) {	
 		//Definicion
 		int []tNotas={5,4,8,10};
-		int numero=4;
+		int numero=8;
 		boolean enc=false;
 		//pedir numero y comprobar si se encuentra en el array
 		for(int i=0;i<tNotas.length;i++){			
@@ -17,13 +17,20 @@ public class VerificarNumero {
 			}
 		}
 		if(enc==true){
-			System.out.print("Encontrado");
+			System.out.print("Encontrado\n");
 		}else{
-			System.out.print("No encontrado");
+			System.out.print("No encontrado\n");
 		}		
 		//pedir numero y mostrar en el primer indice en el que se encuentra
+		for(int i=0;i<tNotas.length;i++){
+			if(tNotas[i]==numero){
+				System.out.println(i);
+				break;
+			}
+		}
 		//El primer indice seria en la posicion 0
-		System.out.print(tNotas.length+"");
+		//System.out.println(tNotas.length+"");
 		//pedir numero y mostrar los indices en los que se encuentra
+		
 	}//main
 }//class//http://jarroba.com/ordenar-arrays-en-java-con-sus-indices/
