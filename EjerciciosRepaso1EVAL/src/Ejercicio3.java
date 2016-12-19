1d8c5efded4867e4c9c6cb01ec2de886c35bd600
DW31 <ik012982i9@H012982.NET>
2016-12-19 13:58:30 +0100

    repaso acbaado

--- EjerciciosRepaso1EVAL/src/Ejercicio3.java
+++ EjerciciosRepaso1EVAL/src/Ejercicio3.java
@@ -5,20 +5,19 @@ el orden inverso al introducido.*/
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		//Variables
-		int nota;
-		final int MAX_LENGHT=10;
-		int[] Numeros=new int [MAX_LENGHT];
 		//Iniciar scanner
 		Scanner sc=new Scanner(System.in);
-		for(int i=MAX_LENGHT-1;i<=0;i--){
-			System.out.println("Introduce una nota: ");
+		//Variables
+		final int MAX_LENGHT=10;
+		int Numeros[]=new int [MAX_LENGHT];
+		
+		for(int i=MAX_LENGHT-1;i>=0;i--){
+			System.out.println("Introduce un numero: ");
 			Numeros[i]=sc.nextInt();
-		}
-		for(int i=0;i<=MAX_LENGHT;i++){
-			System.out.print(Numeros[i]);
-		}
+		}//for
+		for(int i=0;i<MAX_LENGHT;i++){
+			System.out.print(Numeros[i]+" ");
+		}//for	
+	}//main
+}//class
 
-	}
-
-}
